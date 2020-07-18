b56f9203e9547380a6d0ce9098bf73918670a325
Samantha Su <samanthasu2003@gmail.com>
2020-07-18 12:21:17 -0700

    Initial Commit

---
+++ OCSA_NHS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCSA_NHS
+//
+//  Created by Samantha Su on 7/18/20.
+//  Copyright © 2020 samsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
