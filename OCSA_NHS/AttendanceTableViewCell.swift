f1302de223aaadfa98eab64dc1e2ccf5d9f70d30
Samantha Su <samanthasu2003@gmail.com>
2020-09-24 22:23:56 -0700

    started working on attendance

---
+++ OCSA_NHS/AttendanceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AttendanceTableViewCell.swift
+//  OCSA_NHS
+//
+//  Created by Samantha Su on 9/24/20.
+//  Copyright © 2020 samsu. All rights reserved.
+//
+
+import UIKit
+
+class AttendanceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
